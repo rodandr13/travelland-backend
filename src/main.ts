eaa55ad404551fdbeaa79af24acefc5dfab4571a
Andrey Roditelev <rodandr13@gmail.com>
2024-08-01 22:16:24 +0300

    reafactor: add module dir

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
 
-import { AppModule } from './app.module';
+import { AppModule } from './modules/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -5,10 +5,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ExternalModule } from './external/external.module';
+import { NotificationModule } from './notification/notification.module';
 import { OrderModule } from './order/order.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
-import { NotificationModule } from './notification/notification.module';
 
 @Module({
   imports: [
